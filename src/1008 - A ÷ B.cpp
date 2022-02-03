77ffd9e86c1af4b28fe9d8f0d7e0aa6727dd295d
Woojin Kim <woojinkim@unist.ac.kr>
2022-02-03 12:45:37 +0900

    Upload 1008 - A ÷ B

---
+++ src/1008 - A ÷ B.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+double a, b;
+ 
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.precision(12);
+    cin >> a >> b;
+    cout << a/b << "\n";
+    
+    return 0;
+}
