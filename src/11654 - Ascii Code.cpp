4a63367cb7500d1c42d0400c9332734d8b948228
Woojin Kim <woojinkim@unist.ac.kr>
2022-02-03 12:55:42 +0900

    Upload 11654 - Ascii Code

---
+++ src/11654 - Ascii Code.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+char a;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> a;
+	cout << (int)a << '\n';
+	
+    return 0;
+}
