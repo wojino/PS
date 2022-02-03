922a118048c223d07aa7663fb09d4584ee05aca3
Woojin Kim <woojinkim@unist.ac.kr>
2022-02-03 12:44:55 +0900

    Upload 1001 - A - B

---
+++ src/1001 - A - B.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a, b;
+ 
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>> a >> b;
+    cout << a+b << "\n";
+    
+    return 0;
+}
